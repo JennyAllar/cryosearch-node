0053359004394412c79e2016c90086171f658a47
Jenny Allar <jennyallar@gmail.com>
2015-05-18 20:59:00 -0600

    Crud users.

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,48 @@ router.get('/helloworld', function(req, res) {
   res.render('helloworld', { title: 'Hello, World' })
 });
 
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+  res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+  // Internal DB variable
+  var db = req.db;
+
+  // Get form values.
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  // Set collection
+  var collection = db.get('usercollection');
+
+  // Submit to DB
+  collection.insert({
+    "username" : userName,
+    "email" : userEmail
+  }, function (err, doc) {
+    if (err) {
+      // If it failed, return error
+        res.send("There was a problem adding the information to the database");
+    }
+    else {
+      // If it worked, set the header to the address bar isn't /adduser
+      res.location("userlist");
+      // And forward to success page
+      res.redirect("userlist");
+    }
+  });
+});
 module.exports = router;
